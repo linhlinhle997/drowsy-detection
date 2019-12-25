73a99dd889cf0f1779a2deb3946a92702a0b50ad
linhlinhle997 <linhlinh.le997@gmail.com>
2019-12-25 17:28:54 +0700

    commit

--- converttxt.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def convert(txt):
-    mycolnames = ['Datetime','Face','Eye','Path']
-    df = pd.read_csv(txt, sep="|")
-    df.columns = mycolnames
-    df.to_csv("test.csv", sep=",")
